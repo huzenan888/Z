1ddd639b741f8ac656ef79ce88b9ffafc5694e4e
hu-ze-nan <14252383+hu-ze-nan@user.noreply.gitee.com>
2024-04-16 17:37:34 +0800

    试验
    Signed-off-by: nan <1794600469@qq.com>

--- src/hu/task.java
+++ src/hu/task.java
@@ -1,9 +1,10 @@
 package hu;
 
 public class task {
-    public static void main(String[] args) {
-        System.out.println("姓名：胡泽南");
-        System.out.println("班级：22物联网03");
-        System.out.println("学号:2022133382");
+        public static void main(String[] args) {
+            System.out.println("姓名：胡泽南");
+            System.out.println("班级：22物联网03");
+            System.out.println("学号:2022133382");
+        }
     }
-}
+
