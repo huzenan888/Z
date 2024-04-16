f5fde1b08b5c68df639e6fa6bc30e2080d2d3931
nan <1794600469@qq.com>
2024-04-16 15:03:58 +0800

    图形化界面第三次提交

---
+++ src/hu/task.java
@@ -0,0 +1,9 @@
+package hu;
+
+public class task {
+    public static void main(String[] args) {
+        System.out.println("姓名：胡泽南");
+        System.out.println("班级：22物联网03");
+        System.out.println("学号:2022133382");
+    }
+}
